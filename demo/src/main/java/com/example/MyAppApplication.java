68dcc0458e5119e6c77099d857787b299a8bc242
cyj <cyj@goodmit.co.kr>
2022-09-28 14:06:56 +0900

    Initial commit

---
+++ demo/src/main/java/com/example/MyAppApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = "com.example.mylibrary")
+@RestController
+public class MyAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyAppApplication.class, args);
+    }
+}
